2e2cffffa0c176614c63e520184b786a0fdb3dea
Breno Brito <breno.silvabrito@hotmail.com>
2023-02-14 12:26:21 +0000

    Connect to maps with leafletjs

--- script.js
+++ script.js
@@ -14,9 +14,21 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
-      const {latitude} = GeolocationPosition.coors;
-      const {longitude} = GeolocationPosition.coords;
-      console.log(latitude, longitude)
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 14);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');
